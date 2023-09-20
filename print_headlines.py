0d3d76a977e0cb2d0fe89f2f7cd7afd480ad33cd
Faris <fariskarim2000@gmail.com>
2023-09-20 17:57:23 -0400

    set table length to 10

--- print_headlines.py
+++ print_headlines.py
@@ -8,33 +8,44 @@ from prettytable import PrettyTable, ALL
 
 def print_headlines_table(*sources):
     table = PrettyTable()
-    table._max_width = {"BBC": 40, "CNN": 40, "FOX": 40, "NYT": 40, "ArsTechnica": 40}
+    table._max_width = {"BBC": 40, "CNN": 40,
+                        "FOX": 40, "NYT": 40, "ArsTechnica": 40}
 
     if 'BBC' in sources:
         bbc_headlines = fetch_news_bbc()
         table.add_column("BBC", bbc_headlines)
         while len(bbc_headlines) < 10:
             bbc_headlines.append("")
+        while len(bbc_headlines) > 10:
+            bbc_headlines.pop()
     if 'CNN' in sources:
         cnn_headlines = fetch_news_cnn()
         while len(cnn_headlines) < 10:
             cnn_headlines.append("")
+        while len(cnn_headlines) > 10:
+            cnn_headlines.pop()
         table.add_column("CNN", cnn_headlines)
     if 'FOX' in sources:
         fox_headlines = fetch_fox_news()
         table.add_column("FOX", fox_headlines)
         while len(fox_headlines) < 10:
             fox_headlines.append("")
+        while len(fox_headlines) > 10:
+            fox_headlines.pop()
     if 'NYT' in sources:
         nytimes_headlines = fetch_news_nytimes()
         table.add_column("NYT", nytimes_headlines)
         while len(nytimes_headlines) < 10:
             nytimes_headlines.append("")
+        while len(nytimes_headlines) > 10:
+            nytimes_headlines.pop()
     if 'ArsTechnica' in sources:
         ars_headlines = fetch_ars_headlines()
         table.add_column("ArsTechnica", ars_headlines)
         while len(ars_headlines) < 10:
             ars_headlines.append("")
+        while len(ars_headlines) > 10:
+            ars_headlines.pop()
 
     table.hrules = ALL
     print(table)
